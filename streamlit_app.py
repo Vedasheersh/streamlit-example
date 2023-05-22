03bbced923fa0444b57c61f2b782e47bc7bbbee1
Veda Sheersh Boorla <mailforveda@gmail.com>
2023-05-22 03:35:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,18 +29,6 @@ with tab1:
     organism_id = st.text_input("Organism NCBI Taxonomy id:", value="541")
     clicked = st.button("Calculate")
     
-    
-with row0_1:
-    st.title('BuLiAn - Bundesliga Analyzer')
-with row0_2:
-    st.text("")
-    st.subheader('Streamlit App by [Tim Denzler](https://www.linkedin.com/in/tim-denzler/)')
-row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
-with row3_1:
-    st.markdown("Hello there! Have you ever spent your weekend watching the German Bundesliga and had your friends complain about how 'players definitely used to run more' ? However, you did not want to start an argument because you did not have any stats at hand? Well, this interactive application containing Bundesliga data from season 2013/2014 to season 2019/2020 allows you to discover just that! If you're on a mobile device, I would recommend switching over to landscape for viewing ease.")
-    st.markdown("You can find the source code in the [BuLiAn GitHub Repository](https://github.com/tdenzl/BuLiAn)")
-    st.markdown("If you are interested in how this app was developed check out my [Medium article](https://tim-denzler.medium.com/is-bayern-m%C3%BCnchen-the-laziest-team-in-the-german-bundesliga-770cfbd989c7)")
-    
 with tab2:
     st.header("Calculate Ki of an Enzyme-Inhibitor pair")
     enzyme = st.text_input("Enzyme EC number :", value="1.1.1.1")
